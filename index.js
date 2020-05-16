07c52bb69ade6dd0c903d6552574d0e15638d034
BastisBastis <65193082+BastisBastis@users.noreply.github.com>
2020-05-16 12:57:52 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const app = express();
 
-
 app.set('view engine', 'pug');
+
+// serve static files from the `public` folder
+app.use(express.static(__dirname + '/public'));
+
 
 app.get('/', (req, res) => {
   res.render('index', {
